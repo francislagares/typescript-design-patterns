28286f0282a83cf2327db9ea59d09f930e1ef577
Francis Lagares <francis.lagares@gmail.com>
2021-11-20 20:42:20 +0100

    feat: builder pattern implementation

---
+++ src/builder/example/client.ts
@@ -0,0 +1,12 @@
+// House Builder Example Code
+import IglooDirector from './igloo-director';
+import CastleDirector from './castle-director';
+import HouseBoatDirector from './houseBoat-director';
+
+const IGLOO = IglooDirector.construct();
+const CASTLE = CastleDirector.construct();
+const HOUSEBOAT = HouseBoatDirector.construct();
+
+console.log(IGLOO.construction());
+console.log(CASTLE.construction());
+console.log(HOUSEBOAT.construction());
