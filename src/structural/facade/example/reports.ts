8195471b096c8fb4f92a49dbc5bdfacd37bf0dd3
Francis Lagares <francis.lagares@gmail.com>
2021-11-24 21:13:56 +0100

    feat: facade pattern implementation

---
+++ src/structural/facade/example/reports.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-constructor-return */
+/* eslint-disable no-use-before-define */
+// A Singleton Dictionary of Reported Events
+
+export default class Reports {
+  static instance: Reports;
+
+  #reports: { [id: string]: [number, string] } = {};
+
+  #rowId = 0;
+
+  constructor() {
+    if (Reports.instance) {
+      return Reports.instance;
+    }
+    Reports.instance = this;
+  }
+
+  getHistory(): { [id: string]: [number, string] } {
+    return this.#reports;
+  }
+
+  logEvent(event: string): boolean {
+    this.#reports[this.#rowId] = [Date.now(), event];
+    this.#rowId += 1;
+    return true;
+  }
+}
