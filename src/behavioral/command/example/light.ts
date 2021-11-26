eb239a9f551cec815a5e6f0f77acdf03e8b3eb2b
Francis Lagares <francis.lagares@gmail.com>
2021-11-26 21:35:25 +0100

    feat: command pattern implementation

---
+++ src/behavioral/command/example/light.ts
@@ -0,0 +1,14 @@
+/* eslint-disable class-methods-use-this */
+// The Light. The Receiver
+
+export default class Light {
+  turnOn(): void {
+    // A set of instructions to run
+    console.log('Light turned ON');
+  }
+
+  turnOff(): void {
+    // A set of instructions to run
+    console.log('Light turned OFF');
+  }
+}
