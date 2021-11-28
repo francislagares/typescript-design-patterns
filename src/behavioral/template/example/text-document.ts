af0f83805b95fd85c696c30dc5f4f279807318d3
Francis Lagares <francis.lagares@gmail.com>
2021-11-28 14:52:45 +0100

    feat: template pattern implementation

---
+++ src/behavioral/template/example/text-document.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable class-methods-use-this */
+import { Document, AbstractDocument } from './abstract-document';
+
+export default class TextDocument extends AbstractDocument {
+  title(document: Document): void {
+    document.title = 'New Text Document';
+  }
+
+  text(document: Document, text: string): void {
+    document.text = text;
+  }
+
+  footer(document: Document): void {
+    document.footer = '-- Page 1 --';
+  }
+}
